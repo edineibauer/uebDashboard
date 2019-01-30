4c2fee573ecf53107fc17c90a39579fb674930ea
Edinei <galera.org@gmail.com>
2019-01-30 10:24:26 -0200

    add DevPage in 1 1

--- public/assets/dashboard.js
+++ public/assets/dashboard.js
@@ -30,7 +30,17 @@ function menuDashboard() {
         templates = r[3];
         let menu = [];
         let indice = 1;
-        menu.push({icon: "timeline", title: "Dashboard", table: false, link: false, form: false, page: true, file: "dashboardPages/panel", lib: "dashboard", indice: 0});
+        menu.push({
+            icon: "timeline",
+            title: "Dashboard",
+            table: false,
+            link: false,
+            form: false,
+            page: true,
+            file: "dashboardPages/panel",
+            lib: "dashboard",
+            indice: 0
+        });
 
         dbLocal.exeRead("__menu", 1).then(m => {
             $.each(m, function (nome, dados) {
@@ -39,9 +49,20 @@ function menuDashboard() {
 
             //dicionarios
             $.each(dicionarios, function (entity, meta) {
-                if(typeof allow !== "undefined" && typeof allow[entity] !== "undefined" && typeof allow[entity]['menu'] !== "undefined" && allow[entity]['menu']) {
+                if (typeof allow !== "undefined" && typeof allow[entity] !== "undefined" && typeof allow[entity]['menu'] !== "undefined" && allow[entity]['menu']) {
                     nome = ucFirst(entity.replaceAll("_", " ").replaceAll("-", " "));
-                    menu.push({indice: indice, icon: (info[entity]['icon'] !== "" ? info[entity]['icon'] : "storage"), title: nome, table: true, link: false, form: false, page: false, file: '', lib: '',  entity: entity});
+                    menu.push({
+                        indice: indice,
+                        icon: (info[entity]['icon'] !== "" ? info[entity]['icon'] : "storage"),
+                        title: nome,
+                        table: true,
+                        link: false,
+                        form: false,
+                        page: false,
+                        file: '',
+                        lib: '',
+                        entity: entity
+                    });
                     indice++;
                 }
             });
@@ -55,6 +76,20 @@ function menuDashboard() {
             $.each(menu, function (i, m) {
                 $("#dashboard-menu").append(Mustache.render(tpl, m));
             })
+            if (getCookie("setor") === "1" && getCookie("nivel") === "1") {
+                $("#dashboard-menu").append(Mustache.render(tpl, {
+                    "icon": "settings_ethernet",
+                    "title": "DEV",
+                    "link": true,
+                    "table": false,
+                    "page": false,
+                    "form": false,
+                    "lib": "ui-dev",
+                    "file": "UIDev",
+                    "entity": "",
+                    "indice": 100
+                }));
+            }
         })
     });
 }
