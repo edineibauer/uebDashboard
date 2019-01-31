bb34808a886488010bfe1231eda4725e9cff9646
Edinei <galera.org@gmail.com>
2019-01-31 11:57:43 -0200

    ajustes layoult

--- public/view/dashboard.php
+++ public/view/dashboard.php
@@ -7,10 +7,10 @@ if (empty($_SESSION['userlogin'])) {
     ?>
     <nav class="sidebar z-depth-2 collapse color-gray-light no-select animate-left dashboard-nav space-header"
          id="mySidebar">
-        <div class="container row" style="background-color: #e9e9e9">
+        <div class="container row padding-4" style="background-color: #e9e9e9">
             <?php
             if (isset($_SESSION['userlogin']['imagem']) && !empty($_SESSION['userlogin']['imagem'])) {
-                echo '<div class="col" style="height: 60px; width: 60px"><img src="' . HOME . 'image/' . str_replace(HOME, '', $_SESSION['userlogin']['imagem']) . '&w=100&h=100" width="72" height="72" style="margin-bottom:0!important; width: 72px;height: 72px" class="card margin-right"></div>';
+                echo '<div class="col" style="height: 60px; width: 60px"><img src="' . HOME . 'image/' . str_replace(HOME, '', $_SESSION['userlogin']['imagem']) . '&w=80&h=80" width="60" height="60" style="width: 60px;height: 60px;" class="card margin-right"></div>';
             } else {
                 echo '<div class="col" style="height: 60px; width: 60px"><i class="material-icons font-jumbo">people</i></div>';
             }
