bd2c9ceabd2aa85acdc536c4b44b4c583adf0d9d
Edinei <galera.org@gmail.com>
2019-02-02 19:01:43 -0200

    ajustes

--- public/view/panel.php
+++ public/view/panel.php
@@ -4,8 +4,8 @@
             <b><i class="material-icons left padding-right">dashboard</i> <span
                         class="left">Meu Painel</span></b>
         </h5>
-        <div class="right padding-small padding-16">
-            <h5 class="s-font-small"><b class="panel-name upper"></b>, Seja Bem-Vindo(a)</h5>
+        <div class="right padding-small">
+            <h5 class="s-font-small padding-16"><b class="panel-name upper"></b>, Seja Bem-Vindo(a)</h5>
         </div>
     </header>
     <div class="col padding-small dashboard-panel"></div>
