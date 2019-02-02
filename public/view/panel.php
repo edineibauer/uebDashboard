e3c21e9fded3dad12f3a88a8b1a0d4165a553c34
Edinei <galera.org@gmail.com>
2019-02-02 18:56:38 -0200

    ajustes

--- public/view/panel.php
+++ public/view/panel.php
@@ -1,12 +1,12 @@
 <div class="col relative">
     <header class="container">
-        <h5 class="left">
+        <h5 class="left padding-small padding-16">
             <b><i class="material-icons left padding-right">dashboard</i> <span
                         class="left">Meu Painel</span></b>
         </h5>
-        <div class="right padding-small">
+        <div class="right padding-small padding-16">
             <h5 class="s-font-small"><b class="panel-name upper"></b>, Seja Bem-Vindo(a)</h5>
         </div>
     </header>
-    <div class="col padding-small padding-16 dashboard-panel"></div>
+    <div class="col padding-small dashboard-panel"></div>
 </div>
