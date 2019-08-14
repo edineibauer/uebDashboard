5695e3dedec80b71c0eb03941d8b854b3954f7de
Edinei J. Bauer <galera.org@gmail.com>
2019-08-13 21:17:58 -0300

    space dashboard info

--- public/view/panel.php
+++ public/view/panel.php
@@ -1,10 +1,10 @@
 <div class="col relative">
     <header class="container">
-        <h5 class="left padding-small padding-16">
+        <h5 class="left padding-small padding-16" style="margin: 1%">
             <b><i class="material-icons left padding-right">dashboard</i> <span
                         class="left">Meu Painel</span></b>
         </h5>
-        <div class="right padding-small">
+        <div class="right padding-small" style="margin: 1%">
             <h5 class="s-font-small padding-16"><b class="panel-name upper"></b>, Seja Bem-Vindo(a)</h5>
         </div>
     </header>
