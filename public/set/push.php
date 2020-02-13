d19289258649704a8133910815dfd24f98c4fd6b
Edinei J. Bauer <galera.org@gmail.com>
2020-02-13 16:47:31 -0300

    just update push when logged

--- public/set/push.php
+++ public/set/push.php
@@ -4,18 +4,20 @@ use Conn\Create;
 use Conn\Delete;
 use Conn\Read;
 
-$d = ["usuario" => $_SESSION['userlogin']['id']];
-$d['subscription'] = filter_input(INPUT_POST, 'push', FILTER_DEFAULT);
-$d['code'] = filter_input(INPUT_POST, 'p1', FILTER_DEFAULT);
-$d['code'] .= filter_input(INPUT_POST, 'p2', FILTER_DEFAULT);
-$d['code'] .= filter_input(INPUT_POST, 'p3', FILTER_DEFAULT);
+if(!empty($_SESSION['userlogin'])) {
+    $d = ["usuario" => $_SESSION['userlogin']['id']];
+    $d['subscription'] = filter_input(INPUT_POST, 'push', FILTER_DEFAULT);
+    $d['code'] = filter_input(INPUT_POST, 'p1', FILTER_DEFAULT);
+    $d['code'] .= filter_input(INPUT_POST, 'p2', FILTER_DEFAULT);
+    $d['code'] .= filter_input(INPUT_POST, 'p3', FILTER_DEFAULT);
 
-$read = new Read();
-$read->exeRead("push_notifications", "WHERE usuario = :a AND code = :c", "a={$d['usuario']}&c={$d['code']}");
-if($read->getResult()) {
-    $del = new Delete();
-    $del->exeDelete("push_notifications", "WHERE usuario = :a AND code = :c", "a={$d['usuario']}&c={$d['code']}");
+    $read = new Read();
+    $read->exeRead("push_notifications", "WHERE usuario = :a AND code = :c", "a={$d['usuario']}&c={$d['code']}");
+    if ($read->getResult()) {
+        $del = new Delete();
+        $del->exeDelete("push_notifications", "WHERE usuario = :a AND code = :c", "a={$d['usuario']}&c={$d['code']}");
+    }
+
+    $create = new Create();
+    $create->exeCreate("push_notifications", $d);
 }
-
-$create = new Create();
-$create->exeCreate("push_notifications", $d);
