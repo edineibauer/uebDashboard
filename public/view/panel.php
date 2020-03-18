6473d74a960f732f976d456026e812dd71dd34ba
Edinei J. Bauer <galera.org@gmail.com>
2020-03-18 20:37:49 -0300

    painel mobile fix padding

--- public/view/panel.php
+++ public/view/panel.php
@@ -1,12 +1,12 @@
 <div class="col relative">
-    <header class="container">
-        <h5 class="left padding-32">
+    <header class="container s-padding-small">
+        <h5 class="padding-32 s-padding-12 s-padding-tiny">
             <b>
                 <i class="material-icons left padding-right">dashboard</i>
                 <span class="left">Meu Painel</span>
             </b>
         </h5>
-        <div class="right" style="padding-top: 30px">
+        <div class="right s-hide" style="padding-top: 30px">
             <h2 class="col color-text-gray padding-0">
                 <i class="material-icons left padding-small">notification_important</i>
                 <div class="left font-large padding-small">Suas notificações</div>
