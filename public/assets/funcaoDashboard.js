35f0951025f1c16f7618e3f792fd7e88f1fcf6c9
Edinei J. Bauer <galera.org@gmail.com>
2020-05-07 11:17:55 -0300

    graficos

--- public/assets/funcaoDashboard.js
+++ public/assets/funcaoDashboard.js
@@ -1,4 +1,4 @@
-var updateRelatiosCard = null;
+var updateRelatiosCard = null, graficoData = {}, relevant = {};
 
 function hide_sidebar_small() {
     if (screen.width < 993) {
@@ -189,6 +189,78 @@ async function dashboardPanel() {
     }
 }
 
+function getRelevant(entity, dados) {
+    for (let i in dicionarios[entity]) {
+        if (dicionarios[entity][i].format === relevant[0])
+            return dados[dicionarios[entity][i].column] || "não definido";
+    }
+}
+
+async function getGraficoData(grafico) {
+    if (typeof graficoData[grafico.entity] !== "undefined") {
+        if (typeof dicionarios[grafico.entity] !== "undefined" && typeof dicionarios[grafico.entity][grafico.x] !== "undefined" && dicionarios[grafico.entity][grafico.x].key === "relation") {
+            for (let e in graficoData[grafico.entity]) {
+                let id = parseInt(graficoData[grafico.entity][e][grafico.x]);
+                if (!isNaN(id)) {
+                    let relation = dicionarios[grafico.entity][grafico.x].relation;
+                    if (typeof graficoData[relation] === "undefined" || typeof graficoData[relation][id] === "undefined") {
+                        return db.exeRead(relation, id).then(rel => {
+                            if (typeof graficoData[relation] === "undefined")
+                                graficoData[relation] = {};
+
+                            graficoData[relation][id] = rel;
+                            graficoData[grafico.entity][e][grafico.x] = getRelevant(relation, graficoData[relation][id]);
+                            return getGraficoData(grafico);
+                        });
+                    } else {
+                        graficoData[grafico.entity][e][grafico.x] = getRelevant(relation, graficoData[relation][id]);
+                    }
+                }
+            }
+        }
+
+        return Promise.all([]);
+
+    } else {
+        /**
+         * Caso não tenha os dados desta entidade na variável 'graficoData'
+         * busca os dados, adiciona na 'graficoData' e retorna novamente para esta função.
+         */
+        return db.exeRead("relatorios", grafico.report).then($this => {
+            console.log($this);
+            return reportRead($this.entidade, $this.regras, $this.agrupamento, (!isEmpty($this.soma)? JSON.parse($this.soma) : []), (!isEmpty($this.media)? JSON.parse($this.media) : []), $this.ordem, $this.decrescente, 99999999, 0).then(dados => {
+                console.log(dados);
+                graficoData[grafico.entity] = dados.data;
+                return getGraficoData(grafico);
+            })
+        })
+    }
+}
+
+async function showGrafico(grafico) {
+    await getGraficoData(grafico);
+    let size = grafico.size === "100%" ? "12" : (grafico.size === "50%" ? "6" : "4");
+    let $div = $("<div id='grafico-" + grafico.id + "' class='col s" + size + "'></div>").appendTo(".dashboard-panel");
+
+    let g = new Grafico($div[0]);
+    g.setX(grafico.x);
+    g.setY(grafico.y);
+    g.setType(grafico.type);
+    g.setTitle(ucFirst(grafico.entity));
+    g.setOperacao(grafico.operacao);
+    g.setMaximo(grafico.maximo);
+    g.setLabelY(grafico.labely);
+    g.setLabelX(grafico.labelx);
+    g.setMinimoY(grafico.minimoY);
+    g.setMaximoY(grafico.maximoY);
+    g.setMinimoX(grafico.minimoX);
+    g.setMaximoX(grafico.maximoX);
+    g.toogleLegendShow();
+    g.setData(graficoData[grafico.entity]);
+    g.setCornerRounded(grafico.corner);
+    g.show();
+}
+
 $(function () {
     dashboardSidebarInfo();
     dashboardPanel();
@@ -224,5 +296,15 @@ $(function () {
         post('dashboard', 'dash/delete', {id: $this.attr("id")}, function (data) {
             $this.closest("article").parent().remove()
         })
-    })
+    });
+
+    getGraficos().then(graficos => {
+        graficos = orderBy(graficos, "posicao").reverse();
+        dbLocal.exeRead("__relevant", 1).then(relev => {
+            relevant = relev;
+
+            for (let grafico of graficos)
+                showGrafico(grafico);
+        });
+    });
 })
