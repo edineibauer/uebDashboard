c79413fa4b00bf5fdc96a862c58118da16bff2e1
Edinei J. Bauer <galera.org@gmail.com>
2020-05-16 00:25:59 -0300

    fix total mensagens update

--- public/get/openPush.php
+++ public/get/openPush.php
@@ -14,7 +14,7 @@ if (is_numeric($id)) {
         $read->exeRead("enviar_mensagem", "WHERE id = :eid", "eid={$notificacao['enviar_mensagem_id']}");
         if($read->getResult()) {
             $m = $read->getResult()[0];
-            $nTotal = $m['total_de_conversao'] + 1;
+            $nTotal = (!empty($m['total_de_conversao']) && is_numeric($m['total_de_conversao']) && $m['total_de_conversao'] > 0 ? $m['total_de_conversao'] : 0) + 1;
             $up->exeUpdate("enviar_mensagem", ["total_de_conversao" => $nTotal, "taxa_de_conversao" => ((($nTotal * 100) / $m['total_de_entrega'])*100)], "WHERE id = :eid", "eid={$notificacao['enviar_mensagem_id']}");
         }
     }
--- public/get/receivePush.php
+++ public/get/receivePush.php
@@ -14,7 +14,7 @@ if (is_numeric($id)) {
         $read->exeRead("enviar_mensagem", "WHERE id = :eid", "eid={$notificacao['enviar_mensagem_id']}");
         if($read->getResult()) {
             $m = $read->getResult()[0];
-            $nTotal = $m['total_de_entrega'] + 1;
+            $nTotal = (!empty($m['total_de_entrega']) && is_numeric($m['total_de_entrega']) && $m['total_de_entrega'] > 0 ? $m['total_de_entrega'] : 0) + 1;
             $up->exeUpdate("enviar_mensagem", ["total_de_entrega" => $nTotal, "taxa_de_entrega" => ((($nTotal * 100) / $m['total_de_envios'])*100)], "WHERE id = :eid", "eid={$notificacao['enviar_mensagem_id']}");
         }
     }
