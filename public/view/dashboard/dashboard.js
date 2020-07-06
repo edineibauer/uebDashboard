5d7743d0007a5d25e48c5b0ea3c1be153117ba8d
Edinei J. Bauer <galera.org@gmail.com>
2020-07-06 11:19:24 -0300

    include getNotifications

--- public/view/dashboard/dashboard.js
+++ public/view/dashboard/dashboard.js
@@ -162,6 +162,41 @@ function destruct() {
     clearInterval(updateRelatiosCard);
 }
 
+/**
+ * Obtem as notificações
+ * @returns {Promise<[]>}
+ */
+async function getNotifications() {
+    let notifications = await db.exeRead("notifications_report");
+
+    let myNotifications = [];
+    if (!isEmpty(notifications)) {
+        for (let note of notifications) {
+            let notify = await db.exeRead("notifications", note.notificacao);
+            let data = note.data_de_envio;
+
+            if(/T/.test(note.data_de_envio))
+                data = note.data_de_envio.split("T");
+            else if(/\s/.test(note.data_de_envio))
+                data = note.data_de_envio.split(" ");
+            else
+                data = [data, "00:00:00"];
+
+            let hora = data[1].split(":");
+            hora = hora[0] + ":" + hora[1];
+            data = data[0].split("-");
+            data = data[2] + "/" + data[1] + "/" + data[0];
+            notify.data = hora + "\n" + data;
+
+            notify.imagem = notify.imagem || HOME + "assetsPublic/img/favicon-256.png";
+            note.notificacaoData = notify;
+            myNotifications.push(note);
+        }
+    }
+
+    return myNotifications.reverse();
+}
+
 async function updateRelatiosCardInfo() {
     let cards = await get("relatorios_cards_value");
     for(let card of cards)
