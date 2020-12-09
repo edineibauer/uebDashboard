65afff75ea674ca575d3f3d556ab67c60b1094cf
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-09 08:52:30 -0300

    move all notifications to Notifications lib

--- public/post/dash/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
-if($id && $id > 0) {
-    $del = new \Conn\Delete();
-    $del->exeDelete(PRE . "notifications", "WHERE id=:id", "id={$id}");
-}
--- public/react/notifications_report/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$read = new \Conn\Read();
-$read->exeRead("notifications_report", "WHERE notificacao = :idn", "idn={$dados['notificacao']}");
-if(!$read->getResult()) {
-    $del = new \Conn\Delete();
-    $del->exeDelete("notifications", "WHERE id = :id", "id={$dados['notificacao']}");
-}
