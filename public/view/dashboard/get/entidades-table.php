14ecf7f4ca8b04bf4cbe4f9fd23f4a65262d3920
Edinei J. Bauer <edineibauer@gmail.com>
2020-12-29 09:14:00 -0300

    new dashboard

---
+++ public/view/dashboard/get/entidades-table.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$data['data'] = [];
+foreach (\Helpers\Helper::listFolder(PATH_HOME . "entity/cache") as $entity) {
+    if(is_dir($entity) || pathinfo($entity, PATHINFO_EXTENSION) !== "json")
+        continue;
+
+    $entity = str_replace(".json", "", $entity);
+
+    if(\Config\Config::haveEntityPermission($entity, ["menu"]) && file_exists(PATH_HOME . "entity/cache/info/{$entity}.json"))
+        $data['data'][] = ["entidade" => $entity, "info" => json_decode(file_get_contents(PATH_HOME . "entity/cache/info/{$entity}.json"), !0)];
+}
